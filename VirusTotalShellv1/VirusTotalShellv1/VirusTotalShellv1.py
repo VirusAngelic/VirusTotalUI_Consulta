199faa81fd024bd4a392d323a828d0069c0eb841
Moises Diaz <daxter4689@gmail.com>
2022-05-01 13:41:09 -0500

    feature: feature: Agregado dotenv para lectura de apikey y
    Se agrega lectura de archivo .env para almacenado de apikey y path de lectura de archivo que contenera las ips por escanear

--- VirusTotalShellv1/VirusTotalShellv1/VirusTotalShellv1.py
+++ VirusTotalShellv1/VirusTotalShellv1/VirusTotalShellv1.py
@@ -1,13 +1,17 @@
-# Autor: Moises Diaz (git: VirusAngelic)
-import requests #Requests en http
-import csv #For CSV writing
-import json #Handle json outputs
+# Author: Moises Diaz (git: VirusAngelic)
+import os
+
+import requests  # Requests en http
+import csv  # For CSV writing
+import json  # Handle json outputs
 from datetime import datetime
 import sys
+from dotenv import load_dotenv
 
-listAnalisis = list()  
+listAnalisis = list()
+load_dotenv('.env')
 
-def ipScan(apiKey): #Function for a single ip query
+def ipScan(apiKey):  # Function for a single ip query
     ip=input("Ingresa la ip a consultar")
     postIn = requests.get("https://www.virustotal.com/api/v3/ip_addresses/"+look, #making http request
                                  headers={
@@ -16,10 +20,11 @@ def ipScan(apiKey): #Function for a single ip query
     return postIn.text
 
 
-def ipScanFile(apiKey,path): #Scanning ips from file
+def ipScanFile(apiKey, path):
+    # Scanning ips from file
     postInList=[]
-    with open(path,"r") as f:
-        lectura=f.readlines()
+    with open(path, "r") as f:
+        lectura = f.readlines()
         for ind in lectura:
             if ind == "\r":
                 lectura.remove("\r")
@@ -31,11 +36,11 @@ def ipScanFile(apiKey,path): #Scanning ips from file
            postInList.append(postIn.json())
     return postInList
     
-def exportaCSV(analisisJson): #Making CSV from JSON format
-    with open("Analisis.csv","w") as writing:
-        csv_writer = csv.writer(writing, delimiter = ",")
-        count=0
-        ROW_HEADERS=["Ip","Country","ForcePoint","Symantec","Palo Alto Networks"]
+def exportaCSV(analisisJson):  # Making CSV from JSON format
+    with open("Analisis.csv", "w") as writing:
+        csv_writer = csv.writer(writing, delimiter=",")
+        count = 0
+        ROW_HEADERS = ["Ip", "Country", "ForcePoint", "Symantec", "Palo Alto Networks"]
         for keys in ROW_HEADERS:
             csv_writer.writerow(ROW_HEADERS)
         for keys in analisisJson:
@@ -43,23 +48,29 @@ def exportaCSV(analisisJson): #Making CSV from JSON format
             csv_writer.writerow(analisisJson[count]["data"]["attributes"]["country"])
             csv_writer.writerow(analisisJson[count]["data"]["attributes"]["last_analysis_results"]["Forcepoint ThreatSeeker"])
             csv_writer.writerow(analisisJson[count]["data"]["attributes"]["last_analysis_results"]["Forcepoint ThreatSeeker"])
-            count+=1
+            count += 1
 
-def exportaTxt(analisisJson): #Export as txt file
-    with open("Analisis.txt","w") as writing: 
+def exportaTxt(analisisJson):  # Export as txt file
+    with open("Analisis.txt", "w") as writing:
         writing.write(str(analisisJson))
-        return analisisJson #Return for shell operations
+        return analisisJson  # Return for shell operations
 
 def main():
     try:
-        #path=sys.argv[1] #Getting parameter from shel
-        path="ipa.txt"
-        apiKey = "e205541e1ef157f753580d9d866f9bb2d7bac8fbd8d6b5658fff387af5d818f3" #Using a default shell
-        analisis=ipScanFile(apiKey,path) #Calling analisis function with apikey and path argues
-        exportaCSV(analisis) #Making CSV file
-        exportaTxt(analisis) #Making txt file
-    except:
-        print("Ingresa un path de archivo valido")
+
+        APIKEY = os.getenv('VT_APIKEY')
+        PATH = os.getenv('FILE_IPS_PATH')
+        analisis = ipScanFile(APIKEY, PATH)  # Calling analisis function with apikey and path argues
+        if analisis[0]['error']:
+            print('Error en la solicitud a VirusTotal \n ====================  \n', analisis[0]['error']['message'])
+            print('====================')
+        else:
+            exportaCSV(analisis)  # Making CSV file
+            exportaTxt(analisis)  # Making txt file
+
+
+    except :
+        print("Ingresa una ruta de archivo valido")
 
 if __name__== "__main__":
     main()
